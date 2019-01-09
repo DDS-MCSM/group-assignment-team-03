7930e92392eecbef07a980a0ca39aa568938d5c3
vcortada123 <vcortada@gmail.com>
2019-01-10 00:09:29 +0100

    Guarda en un fitxer el nou csv

--- DataClean.R
+++ DataClean.R
@@ -1,7 +1,14 @@
 #setwd("Users/Victor/Desktop/Data Driven/group-assignment-team03")
 phishing <- read.csv("dataset.csv", header = TRUE)
+#names(phishing)
+
 matrixphising <- phishing[, c(1,2,3,4,5,6,7,8,13)]
+#names(matrixphising)
+
 #options(max.print = 150000)
-matrixphising
+
+#matrixphising
 #phishing[0:11056, 2:8]
 
+
+#write.csv(matrixphising, file = "phising.csv")
