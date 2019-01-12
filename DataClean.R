003ca5078d8bc34aef80e59edb7dbf2f3a7ffd54
vcortada123 <vcortada@gmail.com>
2019-01-12 12:15:39 +0100

    Ampliar filas y guardar en fichero los datos finalizados.

--- DataClean.R
+++ DataClean.R
@@ -1,14 +1,13 @@
+#Ruta del fichero
 #setwd("Users/Victor/Desktop/Data Driven/group-assignment-team03")
 phishing <- read.csv("dataset.csv", header = TRUE)
 #names(phishing)
 
 matrixphising <- phishing[, c(1,2,3,4,5,6,7,8,13)]
+
 #names(matrixphising)
-
-#options(max.print = 150000)
-
 #matrixphising
-#phishing[0:11056, 2:8]
 
-
-#write.csv(matrixphising, file = "phising.csv")
+#Ampliar el max de filas para que aparezcan todas
+options(max.print = 150000)
+write.csv(matrixphising, file = "phising.csv")
